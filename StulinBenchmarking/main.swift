b7463308f73e415fb2370a30c22fc6d458da6eee
Scrungo-Beepis <m.lvovsky66@gmail.com>
2018-10-29 13:32:52 -0400

    Initial Commit

---
+++ StulinBenchmarking/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StulinBenchmarking
+//
+//  Created by Misha Lvovsky on 10/29/18.
+//  Copyright © 2018 No. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
